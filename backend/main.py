8572c25bc8283dc0187d9fde5a0536b278c6bd13
tirones-123 <160498925+tirones-123@users.noreply.github.com>
2025-04-18 19:33:42 +0100

    fix: import SessionMiddleware (starlette.middleware.sessions)

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException, status, Request
 from fastapi.middleware.cors import CORSMiddleware
-from starlette.middleware.session import SessionMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 from fastapi.staticfiles import StaticFiles
 import logging
 import os
