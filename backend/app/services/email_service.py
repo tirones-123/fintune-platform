aa2d78f9cd17982c2e752e9c392af572aaa2b756
tirones-123 <160498925+tirones-123@users.noreply.github.com>
2025-04-22 14:43:00 +0100

    ajoutdesmailetfallbakc3

--- backend/app/services/email_service.py
+++ backend/app/services/email_service.py
@@ -112,7 +112,8 @@ def send_notification_email(
         "notification_message": notification_message,
         "notification_type": notification_type,
         "platform_url": settings.FRONTEND_URL,
-        "platform_name": settings.PROJECT_NAME
+        "platform_name": settings.PROJECT_NAME,
+        "logo_url": "https://drive.google.com/uc?export=view&id=1oOmEJx6NPITtgO_TOArsOJs1aekCV1bJ"
         # Add more context if needed, e.g., link to related object
     }
     
